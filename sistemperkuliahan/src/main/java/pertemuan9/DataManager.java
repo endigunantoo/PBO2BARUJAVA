756a7a62faf4e99b563ad2c8ca5f38c9a0bf8a76
A-13 <A-13@A-13>
2024-06-03 11:11:05 +0700

    penambahan oop

---
+++ sistemperkuliahan/src/main/java/pertemuan9/DataManager.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pertemuan9;
+
+/**
+ *
+ * @author A-13
+ */
+class DataManager {
+
+    DataManager(int i, int i0, int i1, int i2) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void displayDosen() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void displayStaf() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void displayMahasiswa() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void displayPerkuliahan() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ sistemperkuliahan/src/main/java/sistemperkuliahan/ui/TextUI.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sistemperkuliahan.ui;
+
+/**
+ *
+ * @author A-13
+ */
+import sistemperkuliahan.ui.TextUI;
+
+import java.util.Scanner;
+public class TextUI {
+
+    public TextUI() {
+    }
+    
+}
